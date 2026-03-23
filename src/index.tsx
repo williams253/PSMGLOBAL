704c9a5991225da89021e68c69558ff1dfea352a
williams253 <williamsgcarmona@gmail.com>
2026-03-23 11:23:18 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import "./index.css";
 import React from "react";
 import { render } from "react-dom";
 import { App } from "./App";
-
+document.title = "PSM GLOBAL C.A. | Soluciones Industriales Integrales";
 render(<App />, document.getElementById("root"));
